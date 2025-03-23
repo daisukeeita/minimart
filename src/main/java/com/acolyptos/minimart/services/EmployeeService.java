920caa3f64c1261f63a01b6de1214476fa7c237d
JAVV <vinas.johnandrei@gmail.com>
2025-03-23 23:37:19 +0800

    adjusting the Employee contructor for more simplicity

--- src/main/java/com/acolyptos/minimart/services/EmployeeService.java
+++ src/main/java/com/acolyptos/minimart/services/EmployeeService.java
@@ -3,6 +3,7 @@ package com.acolyptos.minimart.services;
 import org.bson.types.ObjectId;
 
 import com.acolyptos.minimart.exceptions.DatabaseException;
+import com.acolyptos.minimart.exceptions.ServiceException;
 import com.acolyptos.minimart.models.Employee;
 import com.acolyptos.minimart.repositories.EmployeeRepository;
 
@@ -14,19 +15,23 @@ public class EmployeeService {
   }
 
   public ObjectId createEmployee(String name, String email, ObjectId userId) {
-    Employee employee = new Employee();
-    employee.setName(name);
-    employee.setEmail(email);
-    employee.setUserId(userId);
+    if (name == null || name.trim().isEmpty())
+      throw new IllegalArgumentException("Name is required.");
+    if (email == null || email.trim().isEmpty())
+      throw new IllegalArgumentException("Email is required.");
+    if (userId == null)
+      throw new IllegalArgumentException("userId is required.");
+
+    Employee employee = new Employee(name, email, userId);
 
     try {
       ObjectId result = employeeRepository.insertEmployee(employee);
       System.out.println(employee.getName() + " Successfully added!");
       return result;
-    } catch (DatabaseException e) {
-      System.err.println("Error in service layer: " + e.getMessage());
-      throw e;
-    }
 
+    } catch (DatabaseException exception) {
+      System.err.println("Error in database: " + exception.getMessage());
+      throw new ServiceException("Failed to create employee", exception);
+    }
   }
 }
--- src/main/java/com/acolyptos/minimart/services/UserService.java
+++ src/main/java/com/acolyptos/minimart/services/UserService.java
@@ -6,6 +6,7 @@ import com.acolyptos.minimart.repositories.UserRepository;
 
 import com.acolyptos.minimart.utilities.PasswordUtility;
 import com.acolyptos.minimart.exceptions.DatabaseException;
+import com.acolyptos.minimart.exceptions.ServiceException;
 import com.acolyptos.minimart.exceptions.AuthenticationException;
 
 import org.bson.types.ObjectId;
@@ -17,27 +18,53 @@ public class UserService {
     this.userRepository = new UserRepository();
   }
 
-  public ObjectId createUser(String username, String password, Role role) {
+  public ObjectId createUser(String username, String password, String role) {
+    if (username == null || username.trim().isEmpty())
+      throw new IllegalArgumentException("Username is required.");
+
+    if (password == null || password.trim().isEmpty())
+      throw new IllegalArgumentException("Password is required.");
+
+    if (role == null)
+      throw new IllegalArgumentException("Role is required.");
+
+    if (userRepository.getUserByUsername(username) != null)
+      throw new IllegalArgumentException("Username already exists.");
+
     String hashedPassword = PasswordUtility.hashPassword(password);
 
     User user = new User();
     user.setUsername(username);
     user.setPassword(hashedPassword);
-    user.setRole(role);
+
+    if (Role.EMPLOYEE.toString().equalsIgnoreCase(role)) {
+      user.setRole(Role.EMPLOYEE);
+
+    } else if (Role.MANAGER.toString().equalsIgnoreCase(role)) {
+      user.setRole(Role.MANAGER);
+
+    } else {
+      throw new IllegalArgumentException("Invalid Role Specified.");
+    }
 
     try {
       ObjectId result = userRepository.insertUser(user);
-
       System.out.println(user.getUsername() + " Succesfully added!");
       return result;
+
     } catch (DatabaseException e) {
-      System.err.println("Error in sevice layer: " + e.getMessage());
+      System.err.println("Error in service layer: " + e.getMessage());
       throw e;
     }
 
   }
 
   public User authenticateUser(String username, String password) {
+    if (username == null)
+      throw new IllegalArgumentException("Username is required.");
+    if (password == null)
+      throw new IllegalArgumentException("Password is required.");
+
     try {
       User user = userRepository.getUserByUsername(username);
 
@@ -52,9 +79,9 @@ public class UserService {
       System.out.println("User logged in Succesfully.");
       return user;
 
-    } catch (AuthenticationException e) {
-      System.err.println("Error in authenticating user - " + e.getMessage());
-      throw e;
+    } catch (DatabaseException exception) {
+      System.err.println("Error in authenticating user - " + exception.getMessage());
+      throw new AuthenticationException("Error in Authentication: " + exception.getMessage());
     }
   }
 }
