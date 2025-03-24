be187e3e53facd6be8ad29649189db9079bbba83
JAVV <vinas.johnandrei@gmail.com>
2025-03-24 08:09:45 +0800

    adding comments for documentation and modifying methods for handling exceptions

--- src/main/java/com/acolyptos/minimart/repositories/UserRepository.java
+++ src/main/java/com/acolyptos/minimart/repositories/UserRepository.java
@@ -10,6 +10,8 @@ import com.acolyptos.minimart.exceptions.DatabaseException;
 import com.acolyptos.minimart.exceptions.ResourceNotFoundException;
 import com.acolyptos.minimart.models.User;
 import com.mongodb.MongoException;
+import com.mongodb.MongoQueryException;
+import com.mongodb.MongoTimeoutException;
 import com.mongodb.MongoWriteConcernException;
 import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
@@ -18,9 +20,18 @@ import com.mongodb.client.model.Projections;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.InsertOneResult;
 
+/*
+ * Repository class for managing User-related database operations.
+ * This class provides methods to insert, retrieve, and delete users
+ * from MongoDB database.
+*/
 public class UserRepository {
   private final MongoCollection<User> userCollection;
 
+  /*
+   * Initializes the UserRepository and establishes a connection to
+   * the "users" collection.
+   */
   public UserRepository() {
     this.userCollection = MongoDB.getDatabase().getCollection("users", User.class);
   }
@@ -30,7 +41,9 @@ public class UserRepository {
    *
    * @param user - The user object to be inserted
    * 
-   * @return inserted unique identifier of the User
+   * @return The unique ObjectId of the inserted User.
+   *
+   * @throws DatabaseException if the database error occurs during insertion.
    */
   public ObjectId insertUser(User user) {
     try {
@@ -59,80 +72,139 @@ public class UserRepository {
   }
 
   /*
-   * Retrieves a User by their unique ID
+   * Retrieves a User from the database by their unique ObjectId.
    * 
-   * @param id Expected unique identifier of the user
+   * @param id - The unique ObjectID of the User.
    * 
-   * @return class name and memory address (ex. User@1908201a)
-   * override toString() method as suggested
+   * @return The User object, if found.
+   *
+   * @throws ResourceNotFoundException if no user is found wit hthe given ID.
+   *
+   * @throws DatabaseException if an error occurs during the retrieval process.
    */
   public User getUserById(ObjectId id) {
     try {
       User user = userCollection
           .find(Filters.eq("_id", id))
           .first();
-      if (user == null) {
+
+      if (user == null)
         throw new ResourceNotFoundException("User with id: " + id + " not found.");
-      }
+
       return user;
-    } catch (ResourceNotFoundException e) {
-      throw new DatabaseException("Error retrieving User - " + e.getMessage(), e);
+    } catch (MongoQueryException exception) {
+      throw new DatabaseException("Query Execution Failes: " + exception.getMessage(), exception);
+
+    } catch (MongoTimeoutException exception) {
+      throw new DatabaseException("Database Timeout: " + exception.getMessage(), exception);
+
+    } catch (MongoException exception) {
+      throw new DatabaseException("MongoDB Error: " + exception.getMessage(), exception);
     }
   }
 
   /*
-   * Retrieves a user by their unique ID
+   * Retrieves a User from the database by their username
    * 
-   * @param name Expected name to find the User
+   * @param username - The username of the User.
    * 
-   * @return class name and memory address (ex. User@1908201a)
-   * override toString() method as suggested
+   * @return The User object, if found
+   *
+   * @throws ResourceNotFoundException if no User is found with the given
+   * username.
+   *
+   * @throws DatabaseException if an error occurs during retrieval process.
    */
   public User getUserByUsername(String username) {
     try {
       User user = userCollection
           .find(Filters.eq("username", username))
           .first();
+
       if (user == null) {
         throw new ResourceNotFoundException("User with username " + username + " not found.");
       }
       return user;
-    } catch (ResourceNotFoundException e) {
-      throw new DatabaseException("Error retrieving User - " + e.getMessage(), e);
+    } catch (MongoQueryException exception) {
+      throw new DatabaseException("Query Execution Failed: " + exception.getMessage(), exception);
+
+    } catch (MongoTimeoutException exception) {
+      throw new DatabaseException("Database Timeout: " + exception.getMessage(), exception);
+
+    } catch (MongoException exception) {
+      throw new DatabaseException("MongoDB Error: " + exception.getMessage(), exception);
     }
   }
 
   /*
-   * Retrieves all the Users
+   * Retrieves all Employee documents stored in the database.
    * 
-   * @return lists of all users if found, or null if not found
+   * @return A list of Employee objects.
+   *
+   * @throws ResourceNotFoundException if no employee is found.
+   *
+   * @throws DatabaseException if an error occurs during the retrieval process.
    */
   public List<User> getAllUsers() {
     List<User> users = new ArrayList<>();
 
-    for (User user : userCollection
-        .find()
-        .projection(Projections.include("_id", "role", "username"))) {
-      users.add(user);
-    }
+    // Checks if "users" collection is not empty"
+    User checkIfEmpty = userCollection.find().first();
+    if (checkIfEmpty == null)
+      throw new ResourceNotFoundException("No users found in the database.");
 
-    return users;
+    try {
+      // Iterates through the collecation and adds all found users to the list,
+      // User password is not included on the list.
+      for (User user : userCollection
+          .find()
+          .projection(Projections.include("_id", "role", "username"))) {
+        users.add(user);
+      }
+
+      return users;
+    } catch (MongoQueryException exception) {
+      throw new DatabaseException("Query Execution Failed: " + exception.getMessage(), exception);
+
+    } catch (MongoTimeoutException exception) {
+      throw new DatabaseException("Database Timeount: " + exception.getMessage(), exception);
+
+    } catch (MongoException exception) {
+      throw new DatabaseException("MongoDB Error: " + exception.getMessage(), exception);
+    }
   }
 
   /*
-   * Deletes a user from the collection by their ID
+   * Deletes a User from the database using their unique ObjectId.
    * 
-   * @param id The unique identifier of the User to be deleted
+   * @param id - The unique ObjectId of the User to be delted.
    * 
    * @return true if the deletion was successfull, false otherwise.
+   *
+   * @throws ResourceNotFoundException if no user is found with the given
+   * ObjectId.
+   *
+   * @throws DatabaseException if error occurs during the deletion.
    */
   public boolean deleteUser(ObjectId id) {
     try {
+
+      // Attempts to delete the user and checks if the deletion was successfull.
       DeleteResult result = userCollection.deleteOne(Filters.eq("_id", id));
 
-      return result.getDeletedCount() > 0;
-    } catch (Exception e) {
-      throw new DatabaseException("Failed to delete User.", e);
+      // Checks if user exists based on given unique ObjectId.
+      if (result.getDeletedCount() == 0)
+        throw new ResourceNotFoundException("User not found.");
+
+      return true;
+    } catch (MongoWriteConcernException exception) {
+      throw new DatabaseException("Write Concern Failed: " + exception.getMessage(), exception);
+
+    } catch (MongoWriteException exception) {
+      throw new DatabaseException("Write Failed: " + exception.getError().getMessage(), exception);
+
+    } catch (MongoException exception) {
+      throw new DatabaseException("MongoDB Error: " + exception.getMessage(), exception);
     }
   }
 }
