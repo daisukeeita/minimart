81c4b0717e120347a685e0ebd356787dde8d55a5
JAVV <vinas.johnandrei@gmail.com>
2025-04-22 14:06:18 +0800

    adding service layer for supplier

---
+++ src/main/java/com/acolyptos/minimart/services/SupplierService.java
@@ -0,0 +1,30 @@
+package com.acolyptos.minimart.services;
+
+import org.bson.types.ObjectId;
+import com.acolyptos.minimart.exceptions.DatabaseException;
+import com.acolyptos.minimart.exceptions.ServiceException;
+import com.acolyptos.minimart.models.Supplier;
+import com.acolyptos.minimart.repositories.SupplierRepository;
+
+public class SupplierService {
+
+  private final SupplierRepository supplierRepository;
+
+  public SupplierService () {
+    this.supplierRepository = new SupplierRepository();
+  }
+
+  public ObjectId createSupplier (
+    String name, String address, String contactNumber, String email
+  ) {
+    Supplier supplier = new Supplier(name, address, contactNumber, email);
+
+    try {
+      ObjectId result = supplierRepository.insertSupplier(supplier);
+      return result;
+    } catch (DatabaseException exception) {
+      System.err.println("Error in service layer: " + exception.getMessage());
+      throw new ServiceException("User creation failed: " + exception.getMessage(), exception);
+    }
+  }
+}
