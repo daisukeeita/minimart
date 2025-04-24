384c39065e37d94cbb9b9b4a479c9fc90a696573
JAVV <vinas.johnandrei@gmail.com>
2025-04-24 14:13:40 +0800

    added product service layer

---
+++ src/main/java/com/acolyptos/minimart/services/ProductService.java
@@ -0,0 +1,8 @@
+package com.acolyptos.minimart.services;
+
+import com.acolyptos.minimart.repositories.ProductRepository;
+
+public class ProductService {
+  private final ProductRepository productRepository;
+  private final SupplierService supplierService;
+}
