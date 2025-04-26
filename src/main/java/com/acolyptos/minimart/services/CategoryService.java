3a7a91709976ce1b04fb3bd1fa53a872d4779f3f
JAVV <vinas.johnandrei@gmail.com>
2025-04-27 00:55:08 +0800

    bump

--- src/main/java/com/acolyptos/minimart/services/CategoryService.java
+++ src/main/java/com/acolyptos/minimart/services/CategoryService.java
@@ -16,25 +16,31 @@ public class CategoryService {
     this.categoryRepository =  new CategoryRepository();
   }
 
+  public CategoryService (CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
   public void insertCategory (Category category) {
     String name = category.getName();
     String details = category.getDetails();
 
     try {
       if (name == null) {
+        log.warn("Category name was not provided.");
         throw new IllegalArgumentException(
           "Category name is required."
         );
       }
 
       if (details == null) {
+        log.warn("Category details was not provided.");
         throw new IllegalArgumentException(
           "Details of the category is required."
         );
       } 
 
-      log.info("Category saved successfully.");
       categoryRepository.insertCategory(category);
+      log.info("Category saved successfully.");
     } catch (DatabaseException exception) {
       log.error("Database Error: " + exception.getMessage());
       throw new ServiceException(
