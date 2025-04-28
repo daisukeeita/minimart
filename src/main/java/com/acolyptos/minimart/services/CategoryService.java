753803ea20cdcd8df295cf2bb8525394ac5185a1
JAVV <vinas.johnandrei@gmail.com>
2025-04-28 12:46:56 +0800

    added service layer methods

--- src/main/java/com/acolyptos/minimart/services/CategoryService.java
+++ src/main/java/com/acolyptos/minimart/services/CategoryService.java
@@ -1,15 +1,18 @@
 package com.acolyptos.minimart.services;
 
+import java.util.List;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.acolyptos.minimart.exceptions.DatabaseException;
+import com.acolyptos.minimart.exceptions.ResourceNotFoundException;
 import com.acolyptos.minimart.exceptions.ServiceException;
 import com.acolyptos.minimart.models.Category;
 import com.acolyptos.minimart.repositories.CategoryRepository;
 
 public class CategoryService {
   private final CategoryRepository categoryRepository;
-  private static final Logger log = 
+  private static final Logger LOG = 
     LoggerFactory.getLogger(CategoryService.class);
 
   public CategoryService () {
@@ -25,26 +28,102 @@ public class CategoryService {
     String details = category.getDetails();
 
     try {
-      if (name == null) {
-        log.warn("Category name was not provided.");
+      if (name == null || name.trim().isEmpty()) {
+        LOG.warn("Category name was not provided.");
         throw new IllegalArgumentException(
           "Category name is required."
         );
       }
 
-      if (details == null) {
-        log.warn("Category details was not provided.");
+      if (details == null || details.trim().isEmpty()) {
+        LOG.warn("Category details was not provided.");
         throw new IllegalArgumentException(
           "Details of the category is required."
         );
       } 
 
       categoryRepository.insertCategory(category);
-      log.info("Category saved successfully.");
+      LOG.info("Category saved successfully.");
+
     } catch (DatabaseException exception) {
-      log.error("Database Error: " + exception.getMessage());
+      LOG.error("Database Error: " + exception.getMessage());
       throw new ServiceException(
         "Failed to create category: " + exception.getMessage(),
+        exception
+      );
+    }
+  }
+
+  public Category getCategoryById (ObjectId id) {
+    if (id == null) {
+      LOG.error("Object Id was not provided.");
+      throw new IllegalArgumentException("Id is required.");
+    }
+
+    try {
+      Category category = categoryRepository.getCategoryById(id);
+
+      return category;
+    } catch (ResourceNotFoundException exception) {
+      LOG.error("Category was not found in the database");
+      throw new ServiceException(
+        "Category not found: " + exception.getMessage(), 
+        exception
+      );
+
+    } catch (DatabaseException exception) {
+      LOG.error("Error in database: " + exception.getMessage());
+      throw new ServiceException(
+        "Database Error: " + exception.getMessage(), 
+        exception
+      );
+    }
+  }
+
+  public List<Category> getAllCategories () {
+    try {
+      List<Category> categories = categoryRepository.getAllCategories();
+
+      return categories;
+
+    } catch (ResourceNotFoundException exception) {
+      LOG.error("Empty List: " + exception.getMessage());
+      throw new ServiceException(
+        "List is Empty: " + exception.getMessage(), 
+        exception
+      );
+
+    } catch (DatabaseException exception) {
+      LOG.error("Error in database: " + exception.getMessage());
+      throw new ServiceException(
+        "Database Error: " + exception.getMessage(), 
+        exception
+      );
+    }
+  }
+
+  public boolean deleteCategory (ObjectId id) {
+    if (id == null) {
+      LOG.error("Object Id was not provided.");
+      throw new IllegalArgumentException(
+        "Id is required."
+      );
+    }
+
+    try {
+      return categoryRepository.deleteCategory(id);
+
+    } catch (ResourceNotFoundException exception) {
+      LOG.error("Missing Category:" + exception.getMessage());
+      throw new ServiceException(
+        "Missing Category: " + exception.getMessage(), 
+        exception
+      );
+
+    } catch (DatabaseException exception) {
+      LOG.error("Error in deleting query: " + exception.getMessage());
+      throw new ServiceException(
+        "Delete Execution Failed: " + exception.getMessage(), 
         exception
       );
     }
