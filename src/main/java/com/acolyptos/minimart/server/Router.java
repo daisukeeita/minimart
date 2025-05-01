a9cc012ca1aea6a8b139b64c680bc6ddba805842
JAVV <vinas.johnandrei@gmail.com>
2025-05-01 21:06:04 +0800

    added api for product registration

--- src/main/java/com/acolyptos/minimart/server/Router.java
+++ src/main/java/com/acolyptos/minimart/server/Router.java
@@ -1,5 +1,6 @@
 package com.acolyptos.minimart.server;
 
+import com.acolyptos.minimart.handlers.ProductRegisterHandler;
 import com.acolyptos.minimart.handlers.UserLoginHandler;
 import com.acolyptos.minimart.handlers.UserRegisterHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -8,5 +9,6 @@ public class Router {
   public static void registerRoutes(HttpServer server) {
     server.createContext("/api/login", new UserLoginHandler());
     server.createContext("/api/register", new UserRegisterHandler());
+    server.createContext("/api/addProduct", new ProductRegisterHandler());
   }
 }
